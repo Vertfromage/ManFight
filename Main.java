33c67206865f7ffef577d0e5d0a3847a756b9189
saguins <saralara555@gmail.com>
2019-10-05 15:44:04 +0700

    Add random opponent system
    introduction Robert the Magician!

--- Main.java
+++ Main.java
@@ -1,11 +1,20 @@
 package com.crystal;
 
- public class Main {
+public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 	Man Mike = new Man("Mike");
 	Man Hercules = new Man("Hercules");
+	Man Robert = new Man("Robert", "Magician");
 
-	ManFight mikeAndHercules = new ManFight(Mike,Hercules);
+	int randomNum = (int)(Math.random() * ((1 - 0) + 1)) + 0;
+	switch(randomNum){
+		case 0:
+			ManFight mikeAndHercules = new ManFight(Mike, Hercules);
+			break;
+		case 1:
+			ManFight mikeAndRobert = new ManFight(Mike, Robert);
+			break;
+		}
     }
 }
