1d0d4067dce3feed9cf819b2ddb642c5744b32f6
Bruno <brunocardenasmartinez@gmail.com>
2019-10-26 12:25:16 -0500

    added funny insults

--- src/main/java/com/crystal/Insults.java
+++ src/main/java/com/crystal/Insults.java
@@ -38,9 +38,10 @@ public class Insults {
             "Don't let the door hit you on the way out",
             "You have a pea for a brain",
 	      "Is that you, Medusa?",
+            "you have a frog face",
 	      "Thank god I'm tall so I won't see your face",
-	      "You see that? It's your hope running away",
-	      "you have a frog face"
+	      "You see that? It's your hope running away"
+
     );
     // Mean but keep it pg13.
     static List<String> meanInsults = Arrays.asList(
